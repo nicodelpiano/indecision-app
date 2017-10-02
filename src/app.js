372c032f09a867e501c7844946617935ff9227cc
nicodelpiano <ndel314@gmail.com>
2017-10-01 23:55:00 -0300

    Added webpack dev-server.
    Removed bundle.js.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import IndecisionApp from './components/IndecisionApp'
+import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(
   <IndecisionApp />, document.getElementById('app'));
